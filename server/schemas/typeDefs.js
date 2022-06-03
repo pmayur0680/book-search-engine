c0a195cf2a3c7e7256a872bd4e897a9008901b4e
Mayur Patel <pmayur0680@gmail.com>
2022-06-02 20:07:17 -0500

    gql schema defined in typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Book {    
+    bookId: String!
+    authors: [String]
+    description: String!    
+    title: String!
+    image: String
+    link: String    
+  }  
+
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  input bookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Mutation {
+    createUser(username: String!, email: String!, password String!): Auth
+    login(email: String!, password: String!): Auth    
+    saveBook(input: bookInput): User
+    deleteBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
