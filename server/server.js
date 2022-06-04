726f55767b6bc753013984aab0f3cce3426dcf12
Mayur Patel <pmayur0680@gmail.com>
2022-06-04 15:08:52 -0500

    server.js middleware fix

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware
 });
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
